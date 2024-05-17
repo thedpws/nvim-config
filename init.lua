94d7fe1253b1c0a71f2bb770fc9db4880fa08b57
AZ Vasquez <az.vasquez@pax.us>
2024-05-17 14:56:49 -0400

    Add more plugins!

--- init.lua
+++ init.lua
@@ -3,3 +3,55 @@ require('keymaps')
 require('plugins')
 require('colorscheme')
 require('lsp')
+
+vim.g.mapleader = ' '
+
+function selectOption()
+    local options = {'Option 1', 'Option 2', 'Option 3'}  -- Define a list of options
+    local results = require('telescope.pickers').new({}, {
+        prompt_title = "Select an option",
+        finder = require('telescope.finders').new_table({results = options}),
+        sorter = require('telescope.config').values.generic_sorter({}),
+    }):find()
+    if not results then
+        print("No option selected")
+    else
+        local selected_option = results[1].value
+        print("Selected option: " .. selected_option)
+        -- Process the selected option here
+    end
+end
+
+-- Buffer (b)
+vim.api.nvim_set_keymap('n', '<Leader>bb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>bd', ':bd!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>bp', ':bprev<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[b', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']b', ':bnext<CR>', { noremap = true, silent = true })
+
+-- File (f)
+vim.api.nvim_set_keymap('n', '<Leader>fs', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>pf', ':Telescope find_files<CR>', { noremap = true, silent = true })
+
+-- Apps (x)
+vim.api.nvim_set_keymap('n', '<Leader>xg', ':ChatGPT<CR>', { noremap = true, silent = true })
+
+-- Config (h)
+vim.api.nvim_set_keymap('n', '<Leader>hr', ':source $MYVIMRC<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>hh', ':e $MYVIMRC<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>hf', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<Leader>qq', ':wq<CR>', { noremap = true, silent = true })
+
+-- Projects
+vim.api.nvim_set_keymap('n', '<Leader>pp', ':Telescope projects<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>pg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+
+-- Git (g)
+vim.api.nvim_set_keymap('n', '<Leader>gg', ':Neogit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>gl', ':Neogit log<CR>', { noremap = true, silent = true })
+
+
+
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,11 +12,48 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    'tanvirtin/monokai.nvim',
+
+    'tanvirtin/monokai.nvim', -- Color Scheme
 
     -- LSP manager
     "williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
+
+    { -- ChatGPT
+        'jackMort/ChatGPT.nvim',
+        config = function()
+            require('chatgpt').setup()
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "folke/trouble.nvim",
+            "nvim-telescope/telescope.nvim",
+        }
+    },
+
+    "nvim-telescope/telescope.nvim", -- Fuzzy Finder
+
+    { -- Projects
+        "ahmedkhalf/project.nvim",
+        config = function()
+            require('project_nvim').setup {
+                detection_methods = {'lsp', 'pattern'},
+                patterns = {'.git'}
+            }
+        end
+    },
+
+    {   -- Git
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true
+    },
+
 })
 
